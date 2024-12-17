ea5062ccb5e03c48e99c5b12cc392cd363284487
Alexey Prokopenko <alexey.proko@hotmail.com>
2024-12-17 18:08:01 +0200

    Cleanup code

--- client/src/app/layout/header/header.component.ts
+++ client/src/app/layout/header/header.component.ts
@@ -1,27 +1,12 @@
 import { Component, inject } from '@angular/core';
-import { MatIcon } from '@angular/material/icon';
-import { MatButton } from '@angular/material/button';
-import { MatBadge } from '@angular/material/badge';
 import { MatProgressBar } from '@angular/material/progress-bar';
-import { MatMenu, MatMenuTrigger, MatMenuItem } from '@angular/material/menu';
-import { MatDivider } from '@angular/material/divider';
-import { RouterLink, RouterLinkActive } from '@angular/router';
 import { BusyService } from '../../core/services/busy.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
-    MatIcon,
-    MatButton,
-    MatBadge,
-    RouterLink,
-    RouterLinkActive,
-    MatProgressBar,
-    MatMenuTrigger,
-    MatMenu,
-    MatDivider,
-    MatMenuItem
+    MatProgressBar
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
